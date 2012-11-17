b07edbeead69f78ed44c764200978003880e9454
Jacob Thornton <jacobthornton@gmail.com>
2012-11-16 19:42:45 -0800

    add @ as a valid general url char
    add @ as valid url char
    
    escape @

--- lib/twitter-text/regex.rb
+++ lib/twitter-text/regex.rb
@@ -212,7 +212,7 @@ module Twitter
 
     REGEXEN[:valid_port_number] = /[0-9]+/
 
-    REGEXEN[:valid_general_url_path_chars] = /[a-z0-9!\*';:=\+\,\.\$\/%#\[\]\-_~&|#{LATIN_ACCENTS}]/io
+    REGEXEN[:valid_general_url_path_chars] = /[a-z0-9!\*';:=\@\+\,\.\$\/%#\[\]\-_~&|#{LATIN_ACCENTS}]/io
     # Allow URL paths to contain balanced parens
     #  1. Used in Wikipedia URLs like /Primer_(film)
     #  2. Used in IIS sessions like /S(dfd346)/
